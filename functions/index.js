262e123d4af5a89750b170811040604d95663f81
niawjunior <pasupol@dome.cloud>
2019-07-14 16:37:31 +0700

    line bot for check website status

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
 const functions = require('firebase-functions');
-var express = require('express');
-const axios = require('axios');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-
+const express = require('express');
+const line = require('@line/bot-sdk');
 var app = express();
 app.use(cors({
     origin: true
@@ -13,14 +12,47 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.get('/', (req, res) => {
-    res.status(200).send('ok')
+require('dotenv').config();
+const config = {
+    channelAccessToken: '',
+    channelSecret: ''
+};
+
+const client = new line.Client(config);
+
+var ping = require('ping');
+
+app.post('/api', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
 });
-app.post('/api', (req, res) => {
-        
-});
+
+function handleEvent(event) {
+    console.log(event);
+    if (event.type === 'message' && event.message.type === 'text') {
+        handleMessageEvent(event);
+    } else {
+        return Promise.resolve(null);
+    }
+}
+
+function handleMessageEvent(event) {
+    var msg1 = {
+        type: 'text',
+        text: 'เว็บยังใช้งานไม่ได้'
+    };
+    var http = require('https');
+    http.get('https://pasupol.com', function (res) {
+        return;
+    }).on('error', function(e) {
+        return client.replyMessage(event.replyToken, msg1)    
+    });
+}
 
 exports.bot = functions.https.onRequest(app);
 // app.listen(3000, () => {
-//     console.log('Server Listening on http://localhost:3000/');
+//     console.log('Server Listening on http://localhost:3000/wallet');
 // });
+
+
